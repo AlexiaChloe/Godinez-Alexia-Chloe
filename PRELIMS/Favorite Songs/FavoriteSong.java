8457aff6a3c95ffce7cec79af7c903a4e3a7d0e1
Alexia Chloe <110178915+AlexiaChloe@users.noreply.github.com>
2023-10-24 01:17:28 +0000

    first commit

---
+++ PRELIMS/Favorite Songs/FavoriteSong.java
@@ -0,0 +1,11 @@
+class FavoriteSong{
+   public static void main (String[]args){
+      System.out.println("           Little Things                ");
+      System.out.println("         by: One Direction              ");
+      System.out.println("You can't go to bed without a cup of tea");
+      System.out.println("And maybe that's the reason that you talk in your sleep");
+      System.out.println("And all those conversations are the secrets that I keep");
+      System.out.println("Though it makes no sense to me");
+   }
+
+}
